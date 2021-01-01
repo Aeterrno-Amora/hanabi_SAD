89cc2c89b6462173e483d004c98496b3bcc5e3a5
Aeterrno Amora <13880894553@163.com>
2021-01-01 22:20:19 +0800

    Fix eval_model

--- pyhanabi/tools/eval_model.py
+++ pyhanabi/tools/eval_model.py
@@ -19,7 +19,7 @@ from eval import evaluate
 
 
 def evaluate_legacy_model(
-    weight_files, num_game, seed, bomb, num_run=1, verbose=True
+    weight_files, sad, symnet, num_game, seed, bomb, num_run=1, verbose=True
 ):
     # model_lockers = []
     # greedy_extra = 0
@@ -32,12 +32,8 @@ def evaluate_legacy_model(
             print(
                 "evaluating: %s\n\tfor %dx%d games" % (weight_file, num_run, num_game)
             )
-        if "sad" in weight_file or "aux" in weight_file:
-            sad = True
-        else:
-            sad = False
 
-        device = "cuda:0"
+        device = "cuda:1"
 
         state_dict = torch.load(weight_file)
         input_dim = state_dict["net.0.weight"].size()[1]
@@ -45,7 +41,7 @@ def evaluate_legacy_model(
         output_dim = state_dict["fc_a.weight"].size()[0]
 
         agent = r2d2.R2D2Agent(
-            False, 3, 0.999, 0.9, device, input_dim, hid_dim, output_dim, 2, 5, False
+            False, 3, 0.999, 0.9, device, symnet, input_dim, hid_dim, output_dim, 2, 5, False
         ).to(device)
         utils.load_weight(agent.online_net, weight_file, device)
         agents.append(agent)
@@ -74,8 +70,10 @@ def evaluate_legacy_model(
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--weight", default=None, type=str, required=True)
-    parser.add_argument("--num_player", default=None, type=int, required=True)
+    parser.add_argument("weight", default=None, type=str, required=True)
+    parser.add_argument("--num_player", default=2, type=int)
+    parser.add_argument("--sad", action='store_const', const=True, default=False)
+    parser.add_argument("--symnet", action='store_const', const=True, default=False)
     args = parser.parse_args()
 
     assert os.path.exists(args.weight)
@@ -83,4 +81,4 @@ if __name__ == "__main__":
     weight_files = [args.weight for _ in range(args.num_player)]
 
     # fast evaluation for 10k games
-    evaluate_legacy_model(weight_files, 1000, 1, 0, num_run=10)
+    evaluate_legacy_model(weight_files, sad, symnet, 1000, 1, 0, num_run=10)
